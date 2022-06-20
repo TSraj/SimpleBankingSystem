6198c326eae36fff49b74dae984700993007528d
Md. Tanvir Sadik Raj <32238769+TSraj@users.noreply.github.com>
2022-06-20 11:04:49 +0600

    test one

--- src/Bank/bankingSystem.java
+++ src/Bank/bankingSystem.java
@@ -1,7 +1,16 @@
 package Bank;
 
+import java.util.Scanner;
+
 public class bankingSystem {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		Scanner myScanner = new Scanner(System.in);
+		int a,b;
+		System.out.println("Enter number one: ");
+		a = myScanner.nextInt();
+		System.out.println("Enter number two: ");
+		b = myScanner.nextInt();
+		int result = a+b;
+		System.out.println("The result is: "+result);
 	}
 }
