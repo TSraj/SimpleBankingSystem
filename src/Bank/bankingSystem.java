a36e8d60b2017aa649143d7d26f4dc8b51106f51
Md. Tanvir Sadik Raj <32238769+TSraj@users.noreply.github.com>
2022-06-20 11:06:45 +0600

    modified

--- src/Bank/bankingSystem.java
+++ src/Bank/bankingSystem.java
@@ -1,16 +1,53 @@
 package Bank;
 
-import java.util.Scanner;
-
 public class bankingSystem {
 	public static void main(String[] args) {
-		Scanner myScanner = new Scanner(System.in);
-		int a,b;
-		System.out.println("Enter number one: ");
-		a = myScanner.nextInt();
-		System.out.println("Enter number two: ");
-		b = myScanner.nextInt();
-		int result = a+b;
-		System.out.println("The result is: "+result);
+		
+	}
+	
+		class bankAccount{
+			int balance;
+			int previousTransaction;
+			String customerName;
+			String customerID;
+			
+			
+			bankAccount(String cname, String cid) {
+				customerName = cname;
+				customerID = cid;
+			}
+			
+			//Balance Inforamtion
+			void deposit(int amount) {
+				if (amount !=0) {
+					balance = balance+amount;
+					previousTransaction = amount;
+				}
+			}
+			
+			
+			//Withdrawn Information
+			void withdraw(int amount) {
+				if (amount !=0) {
+					balance = balance-amount;
+					previousTransaction = -amount;
+				}
+			}
+			
+			//Transaction calculation
+			void getPreviousTransction() {
+				if (previousTransaction > 0) {
+					System.out.println("Deposited: "+previousTransaction);
+				}else if (previousTransaction < 0) {
+					System.out.println("Withdrawn: "+Math.abs(previousTransaction));
+				}else {
+					System.out.println("No transction happen");
+				}
+			}
+			
+			//Menu show 
+			void showMenu() {
+				
+			}
 	}
 }
